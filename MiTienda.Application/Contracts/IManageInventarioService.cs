3e621c78301a702232d96b84c43485e806c55f6b
Vigiani Ricardo Nicolas <nicovigiani_kpo2008@hotmail.com>
2024-03-05 23:40:40 -0300

    Agrego inventarioDTO y Servicio en fases iniciales

---
+++ MiTienda.Application/Contracts/IManageInventarioService.cs
@@ -0,0 +1,22 @@
+﻿
+using MiTienda.Application.DTOs;
+using MiTienda.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiTienda.Application.Contracts
+{
+    public interface IManageInventarioService
+    {
+        List<ReturnInventarioDTO> GetInvetarios(int IdSucursal);
+        List<ReturnInventarioDTO> GetInventarioById(int IdInventario);
+        List<ReturnInventarioDTO> GetInventarioByCodigoBarra(string CodigoBarra);
+        //string UpdateCliente(ClienteDTO cliente);
+        //string CreateCliente(ClienteDTO cliente);
+        //string DeleteCliente(int Id);
+
+    }
+}
